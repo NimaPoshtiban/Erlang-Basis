a14683b961f610b32fbdcf63ca84d7c2ffcb6c78
Nima Poshtiban <Poshtibannima@gmail.com>
2021-11-13 15:32:15 +0330

    using third party module

--- chapter02/convert.erl
+++ chapter02/convert.erl
@@ -1,6 +1,8 @@
 -module(convert).
 -export([mps_to_mph/1,mph_to_km/1]).
 
+% use -compile(export_all) . if you need to make all your functions public
+
 mps_to_mph(Mps)-> 2.23693629 * Mps.
 
 mph_to_km(Mph)-> 3.6 * Mph.
