8aaa4336ac4e9df0086ed6acf20160f02670cbe6
Nima Poshtiban <Poshtibannima@gmail.com>
2021-11-14 22:17:28 +0330

    changed functions structure via using tuple

--- chapter03/drop.erl
+++ chapter03/drop.erl
@@ -1,8 +1,12 @@
 -module(drop).
--export([fall_velocity/2]).
+-export([fall_velocity/1]).
 
 % these are three diffrent definition for a function
 % that will be choosen by pattern matching
+
+fall_velocity(Where) ->
+    {Planemo,Distance} = Where,
+    fall_velocity(Planemo,Distance).
 
 fall_velocity(earth, Distance) when Distance >=0 -> math:sqrt(2 * 9.8 * Distance);
 
