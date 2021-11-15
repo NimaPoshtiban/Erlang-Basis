d8f5113e6c04ea565a21016b93f64f55fc813702
Nima Poshtiban <Poshtibannima@gmail.com>
2021-11-15 20:11:55 +0330

    added working with user inputs in a line

--- chapter05/ask.erl
+++ chapter05/ask.erl
@@ -1,5 +1,5 @@
 -module(ask).
--export([term/0]).
+-export([term/0,line/0]).
 -import(drop,[fall_velocity/1]).
 
 term() ->
@@ -21,3 +21,31 @@ process_term({ok,_}) ->
 process_term({error,_}) -> 
     io:format("You must enter a tuple with currect syntax.~n"),
     term().
+
+
+line() ->
+    Planemo = get_planemo(),
+    Distance = get_distance(),
+    fall_velocity({Planemo,Distance}).
+
+get_planemo() ->
+    io:format("Where are you? ~n "),
+    io:format(" 1. Earth ~n "),
+    io:format(" 2. Earth's Moon ~n "),
+    io:format(" 3. Mars ~n "),
+    Answer = io:get_line("Which? > "),
+    Value = hd(Answer),
+    char_to_planemo(Value).
+
+char_to_planemo(Char) ->
+    if
+        [Char] == "1" -> earth;
+        Char == $2 -> moon;
+        Char == 51 -> mars
+    end.
+
+get_distance() ->
+    Input = io:get_line("How far? (meters) > "),
+    Value = string:strip(Input,right,$\n),
+    {Distance,_} = string:to_integer(Value),
+    Distance.
